d43ed3e782aebfe4597794e71596ffd2b2ed41a9
joniborda <jonathanmatiasborda@gmail.com>
2018-09-26 22:13:59 -0300

    intento iterar para saber los que pueden llenar y hasta donde

--- LlenandoEstanque/src/unlam/Vertedero.java
+++ LlenandoEstanque/src/unlam/Vertedero.java
@@ -1,9 +1,12 @@
 package unlam;
 
+import java.util.ArrayList;
+
 public class Vertedero {
 
 	int superficie;
 	int profundidad;
+	int nivelCañeria;
 	int profundidadCañeria;
 	int litrosLlenados;
 	boolean ultimo = false;
@@ -11,21 +14,22 @@ public class Vertedero {
 	public Vertedero(int superficie, int profundidad, int profundidadCañeria) {
 		this.superficie = superficie;
 		this.profundidad = profundidad;
-		this.profundidadCañeria = profundidad - profundidadCañeria;
+		this.nivelCañeria = profundidad - profundidadCañeria;
+		this.profundidadCañeria = profundidadCañeria;
 	}
 	
 	public Vertedero(int superficie, int profundidad) {
 		this.superficie = superficie;
 		this.profundidad = profundidad;
-		this.profundidadCañeria = profundidad;
+		this.nivelCañeria = profundidad;
 		this.ultimo = true;
 	}
 
 	public int llenar(int litrosALlenar) {
 
 		if (this.litrosLlenados < this.superficie * this.profundidad) {
-			if (this.profundidadCañeria * this.superficie < this.litrosLlenados + litrosALlenar) {
-				this.litrosLlenados = this.profundidadCañeria * this.superficie;
+			if (this.nivelCañeria * this.superficie < this.litrosLlenados + litrosALlenar) {
+				this.litrosLlenados = this.nivelCañeria * this.superficie;
 			} else {
 				this.litrosLlenados += litrosALlenar;
 			}
@@ -43,11 +47,21 @@ public class Vertedero {
 	}
 	
 	public boolean cargadoHastaCañeria() {
-		return this.litrosLlenados >= this.superficie * this.profundidadCañeria;
+		return this.litrosLlenados >= this.superficie * this.nivelCañeria;
 	}
 	
 	public String toString() {
 		return "superficie " + this.superficie + " profundidad " + this.profundidad + " llenados " + this.litrosLlenados;
+	}
+	
+	public int puedeLlenar(ArrayList<Vertedero> vertederos, int position) {
+		if (vertederos.get(position).litrosLlenados < this.superficie * this.nivelCañeria) {
+			return this.superficie * this.nivelCañeria - this.litrosLlenados;
+		}
+		
+		
+		
+		return 0;
 	}
 
 }
--- LlenandoEstanque/src/unlam/Vertederos.java
+++ LlenandoEstanque/src/unlam/Vertederos.java
@@ -1,42 +1,72 @@
 package unlam;
 
 import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.Set;
+import java.util.TreeSet;
 
 public class Vertederos {
 
 	private ArrayList<Vertedero> vertederos;
 	private int volumenALlenar;
 	private int vertedoresUsados;
-	
+
 	public Vertederos() {
-		
+
 	}
+
 	public Vertederos(ArrayList<Vertedero> vertederos) {
 		this.vertederos = vertederos;
 	}
-	
+
 	public void add(Vertedero vertedero) {
 		if (this.vertederos == null) {
 			this.vertederos = new ArrayList<Vertedero>();
 		}
 		this.vertederos.add(vertedero);
 	}
-	
+
 	public Vertedero llenar(int volumenALlenar) {
 		this.volumenALlenar = volumenALlenar;
 		int llenado = 0;
 		Vertedero vertedero;
 		Vertedero vertederoAnterior;
+		int mayorAlturaLlenadaEnIteracion = 0;
+		Set<Integer> vertederosQuePuedenCargar = new TreeSet<Integer>();
+
+		for (int i = 0; i < this.vertederos.size(); i++) {
+			vertedero = this.vertederos.get(i);
+			if (vertedero.litrosLlenados < vertedero.superficie * vertedero.nivelCañeria) {
+				vertederosQuePuedenCargar.add(i);
+				
+				if (i == 0 || this.vertederos.get(i - 1).litrosLlenados >= this.vertederos.get(i - 1).superficie
+						* this.vertederos.get(i - 1).nivelCañeria) {
+					
+					mayorAlturaLlenadaEnIteracion = vertedero.profundidadCañeria;
+				}
+				break;
+			} else if (i + 1 < this.vertederos.size()) {
+				if (this.vertederos.get(i + 1).litrosLlenados < this.vertederos.get(i + 1).superficie
+						* this.vertederos.get(i).profundidadCañeria) {
+					
+					mayorAlturaLlenadaEnIteracion = this.vertederos.get(i).profundidadCañeria;
+					vertederosQuePuedenCargar.add(i + 1);
+					break;
+				}
+				vertederosQuePuedenCargar.add(i);
+			}
+		}
+
 		for (int i = 0; i < this.vertederos.size(); i++) {
 			vertedero = this.vertederos.get(i);
 			if (i != 0) {
-				vertederoAnterior = this.vertederos.get(i-1);
+				vertederoAnterior = this.vertederos.get(i - 1);
 				if (vertederoAnterior.cargadoHastaCañeria()) {
 					llenado = vertedero.llenar(this.volumenALlenar);
 				}
 			} else {
 				System.out.println(vertedero);
-				llenado = vertedero.llenar(this.volumenALlenar);				
+				llenado = vertedero.llenar(this.volumenALlenar);
 			}
 			if (llenado != 0) {
 				this.volumenALlenar -= llenado;
@@ -46,11 +76,11 @@ public class Vertederos {
 		}
 		return null;
 	}
-	
+
 	public ArrayList<Vertedero> getVertederos() {
 		return this.vertederos;
 	}
-	
+
 	public int getVertedoresUsados() {
 		return this.vertedoresUsados;
 	}
